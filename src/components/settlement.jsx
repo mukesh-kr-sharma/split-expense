cb9bac475c46cbe484dd968f4712979825c28953
Sharma, Mukesh <musharma@levi.com>
2024-07-22 01:20:27 +0530

    settlement done

--- src/components/settlement.jsx
+++ src/components/settlement.jsx
@@ -1,59 +1,98 @@
 "use client"
 
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table"
-import { ComboBox } from "@/components/combobox";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import MyContext from "@/my-context";
-import { SelectUsersPopover } from "./selectusers";
+
+const absoluteSum = (list) => {
+    let sum = 0
+    for (let i = 0; i < list.length; i++)
+        sum += Math.abs(list[i])
+    return sum
+}
 
 const SettlementSuggestion = () => {
     const { context, setContext } = useContext(MyContext)
+    const [settlements, setSettlements] = useState([])
 
-    const tmp_users = ['mukesh', 'rishabh', 'gourav']
+    useEffect(() => {
+        const users = context['users'] || []
+        const expenses = context['expenses'] || []
 
-    const tmp = [
-        { 'paid_by': 'mukesh', 'amount': 1000, 'participants': ['mukesh', 'rishabh'] },
-        { 'paid_by': 'rishabh', 'amount': 2000, 'participants': ['gourav', 'rishabh'] },
-        { 'paid_by': 'gourav', 'amount': 500, 'participants': ['gourav', 'rishabh'] },
-        { 'paid_by': 'rishabh', 'amount': 1200, 'participants': ['mukesh', 'gourav', 'rishabh'] },
-        { 'paid_by': 'rishabh', 'amount': 3000, 'participants': ['mukesh', 'gourav', 'rishabh'] },
-        { 'paid_by': 'mukesh', 'amount': 600, 'participants': ['mukesh', 'gourav', 'rishabh'] }
-    ]
+        var expense_summary = {}
 
-    var expense_summary = {}
+        users.map((user) => {
+            let total_paid = 0
+            let self_expense = 0
 
-    tmp_users.map((user) => {
-        let total_paid = 0
-        let self_expense = 0
+            expenses.forEach((record) => {
+                if (record['paid_by'] == user)
+                    total_paid = total_paid + parseInt(record['amount'])
 
-        tmp.forEach((record) => {
-            if (record['paid_by'] == user)
-                total_paid = total_paid + record['amount']
+                if (record['participants'].includes(user))
+                    self_expense += Math.round(record['amount'] / record['participants'].length)
 
-            if (record['participants'].includes(user))
-                self_expense += record['amount'] / record['participants'].length
+            })
+
+            let net_amount = total_paid - self_expense
+            expense_summary[user] = net_amount
         })
 
-        let net_amount = total_paid - self_expense
-        expense_summary[user] = net_amount
-    })
+        var tmp_settlements = []
+        var iterCount = 0
+        while (absoluteSum(Object.values(expense_summary)) > users.length && iterCount < 1000) {
+            iterCount += 1
 
-    
-    console.log(expense_summary)
+            let min_key = Object.keys(expense_summary)[0]
+            let min_value = Object.values(expense_summary)[0]
 
+            let max_key = min_key
+            let max_value = min_value
+
+            for (const key in expense_summary) {
+                if (expense_summary[key] === 0) {
+                    delete expense_summary[key]
+                    continue
+                }
+
+                if (expense_summary[key] < min_value) {
+                    min_key = key
+                    min_value = expense_summary[key]
+                }
+                if (expense_summary[key] > max_value) {
+                    max_key = key
+                    max_value = expense_summary[key]
+                }
+            }
+
+            if (Math.abs(min_value) < max_value) {
+                tmp_settlements.push({
+                    'from': min_key,
+                    'to': max_key,
+                    'amount': Math.abs(min_value)
+                })
+                delete expense_summary[min_key]
+                expense_summary[max_key] = min_value + max_value
+            } else {
+                tmp_settlements.push({
+                    'from': min_key,
+                    'to': max_key,
+                    'amount': max_value
+                })
+                delete expense_summary[max_key]
+                expense_summary[min_key] = min_value + max_value
+            }
+        }
+        setSettlements(tmp_settlements)
+    }, [context])
 
     return (
         <Card className="w-[500px] hidden" id="settlement-card">
             <CardHeader>
                 <CardTitle>Settlement</CardTitle>
-                {/* <CardDescription>Add expenses</CardDescription> */}
-
-
-
             </CardHeader>
             <CardContent>
                 <ScrollArea className="h-[400px] w-full rounded-md border">
@@ -66,17 +105,19 @@ const SettlementSuggestion = () => {
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            <TableRow>
-                                <TableCell className="font-medium">
-                                    <Input id="paid-for-input" type="text" className="flex-1" placeholder="Item name" />
-                                </TableCell>
-                                <TableCell>
-                                    wed
-                                </TableCell>
-                                <TableCell className="text-right">
-                                    <Input id="amount-input" type="number" className="flex-1 text-right" placeholder="Amount" />
-                                </TableCell>
-                            </TableRow>
+                            {settlements.map((record, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>
+                                        {record["from"]}
+                                    </TableCell>
+                                    <TableCell>
+                                        {record["to"]}
+                                    </TableCell>
+                                    <TableCell className="text-right">
+                                        Rs. {record["amount"]}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
 
                         </TableBody>
                     </Table>
